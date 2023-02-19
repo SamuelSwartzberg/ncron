523829fc8d02aa1233efc7ac3b905febaa8a7de3
Sam Swartzberg <me@samswartzberg.com>
2023-02-19 20:43:18 +0100

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+
+SCRIPT_DIR=$(cd -- "$(dirname -- "$(realpath "${BASH_SOURCE[0]}")")" &>/dev/null && pwd)
+
+cd "$SCRIPT_DIR" && npm install
+ln -s "$SCRIPT_DIR"/ncron "$HOMEBIN"/ncron
